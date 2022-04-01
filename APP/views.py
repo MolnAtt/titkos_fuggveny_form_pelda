229833ed9e325c4fa570836f6f01025aa6bddc17
MolnAtt <molnar.attila@szlgbp.hu>
2022-04-01 11:47:41 +0200

    befejezés

--- APP/views.py
+++ APP/views.py
@@ -5,21 +5,32 @@ from random import randint
 def kviz(request):
     pla = randint(0,30)
     plb = randint(0,30)
+    fela = randint(0,30)
+    felb = randint(0,30)
     plmo = titkos_fuggveny(pla,plb)
     template = "kviz.html"
     context = {
         'pla':pla, 
-        'plb': plb, 
+        'plb':plb, 
         'plmo':plmo,
-        'fela':randint(0,30),
-        'felb':randint(0,30),
+        'fela':fela,
+        'felb':felb,
+        'volt_valasz': request.method!="GET",
     }
 
     if request.method=="POST":
-        print(int(request.POST['regifela']))
-        print(int(request.POST['regifelb']))
-        print(int(request.POST['felmo']))
-        print(titkos_fuggveny(int(request.POST['regifela']), int(request.POST['regifelb']))==int(request.POST['felmo']))
+        regifela = int(request.POST['regifela'])
+        regifelb = int(request.POST['regifelb'])
+        regifelmo_user_szerint = int(request.POST['felmo'])
+        regifelmo_valojaban = titkos_fuggveny(regifela, regifelb)
+
+        # print(regifela)
+        # print(regifelb)
+        # print(regifelmo_user_szerint)
+        # print(regifelmo_valojaban)
+
+        context['helyes'] = regifelmo_user_szerint == regifelmo_valojaban
+
     return render(request, template, context)
 
 
